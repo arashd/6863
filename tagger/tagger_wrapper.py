4a4dbf822e8dfbad1c33fefa3df3e8ac1a3eb842
Arash Delijani <arashd@Arashs-MacBook-Air.local>
2012-12-15 02:05:49 -0500

    we can tag tweets nowclear! CMU POS tagger, python wrapper.

---
+++ tagger/tagger_wrapper.py
@@ -0,0 +1,28 @@
+import subprocess
+
+class POSTagger(object):
+	# initializes tagger by calling shell script.
+	# the first call basically trains the tagger.
+	def __init__(self):
+		self.p = subprocess.Popen(['/Users/arashd/Dropbox/workspace/6.863/project/tagger/runTagger.sh'], 
+			stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+	# tags the tweet by putting it on stdin and reading 
+	# the result off stdout
+
+	# returns a tuple containing as its first element a list of tags 
+	# and as its second element a list of tag probabilities
+	
+	#  !!!!  don't use the same POS tagger with multiple threads. it will create deadlocks !!!!
+	def tag(self, tweet):
+		self.p.stdin.write(tweet + '\n')
+		split_result = self.p.stdout.readline().split('\t')
+		if len(split_result) != 4:
+			return None
+		return (split_result[1].split(), split_result[2].split())
+
+if __name__ == '__main__':
+	tagger = POSTagger()
+	for i in range(1000):
+		print tagger.tag('This is a random tweet!')
+		
